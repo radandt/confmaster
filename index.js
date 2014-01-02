cd204bdf366172f10ff821361448189352c69240
unknown <Nutzer@.(none)>
2014-01-02 12:30:25 +0100

    added NodeJS-Server and entry-point

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    if (req.url.substr(-3) === '.js') {
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
+    } else if (req.url.substr(-4) === '.css') {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+    } else if (req.url.substr(-5) === '.json') {
+        res.writeHead(200, {'Content-Type': 'application/json'});        
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+    }
+
+    var filename = 'confmaster/' + (req.url === '/' ? 'index.html' : req.url);
+    console.log(filename);
+
+    fs.readFile(filename, function (err, data) {
+        res.end(data);
+    });
+}).listen(8001);
+
+console.log('run server at port 8001');
